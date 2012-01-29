707d1e25bff1c5451d1c6a8720c0384dfe8059ef
Andris Reinman <andris@node.ee>
2012-01-29 16:51:13 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,21 @@
+# mailcomposer
+
+This is going to be a module for generating e-mail messages that can be
+streamed to SMTP etc.
+
+## Running tests
+
+Tests are run with [nodeunit](https://github.com/caolan/nodeunit)
+
+Run
+
+    npm test
+
+or alternatively
+
+    node run_tests.js
+
+
+## License
+
+**MIT**
---
+++ test/mailcomposer.js
@@ -0,0 +1,50 @@
+var testCase = require('nodeunit').testCase,
+    MailComposer = require("../lib/mailcomposer").MailComposer;
+
+exports["General tests"] = {
+    
+    "Create a new MailComposer object": function(test){
+        var mailcomposer = new MailComposer();
+        test.equal(typeof mailcomposer.on, "function");
+        test.equal(typeof mailcomposer.emit, "function");
+        test.done();
+    },
+    
+    "Normalize key names": function(test){
+        var normalizer = MailComposer.prototype.normalizeKey;
+        
+        test.equal(normalizer("abc"), "Abc");
+        test.equal(normalizer("aBC"), "Abc");
+        test.equal(normalizer("ABC"), "Abc");
+        test.equal(normalizer("a-b-c"), "A-B-C");
+        test.equal(normalizer("ab-bc"), "Ab-Bc");
+        test.equal(normalizer("ab-bc-cd"), "Ab-Bc-Cd");
+        test.equal(normalizer("AB-BC-CD"), "Ab-Bc-Cd");
+        test.equal(normalizer("mime-version"), "MIME-Version"); // special case
+        
+        test.done();
+    },
+    
+    "Add header": function(test){
+        var mc = new MailComposer();
+        test.equal(typeof mc._headers["Test-Key"], "undefined");
+        mc.addHeader("test-key", "first");
+        test.equal(mc._headers["Test-Key"], "first");
+        mc.addHeader("test-key", "second");
+        test.deepEqual(mc._headers["Test-Key"], ["first","second"]);
+        mc.addHeader("test-key", "third");
+        test.deepEqual(mc._headers["Test-Key"], ["first","second","third"]);
+        test.done();
+    },
+    
+    "Get header": function(test){
+        var mc = new MailComposer();
+        test.equal(mc.getHeader("MIME-Version"), "1.0");
+        test.equal(mc.getHeader("test-key"), "");
+        mc.addHeader("test-key", "first");
+        test.equal(mc.getHeader("test-key"), "first");
+        mc.addHeader("test-key", "second");
+        test.deepEqual(mc.getHeader("test-key"), ["first", "second"]);
+        test.done();
+    }
+};
